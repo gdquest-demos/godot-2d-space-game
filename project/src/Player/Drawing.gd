6ca20e7d4d24d5cd80a093c24307e90d6f42fb6d
Nathan Lovato <nathan@gdquest.com>
2020-02-01 15:51:17 -0600

    Simplify pirate ship drawing code

--- project/src/Player/Drawing.gd
+++ project/src/Player/Drawing.gd
@@ -8,7 +8,4 @@ onready var collision_shape: CollisionPolygon2D = owner.get_node("CollisionShape
 
 func _draw() -> void:
 	var polygon := collision_shape.polygon
-	var colors := []
-	for p in polygon:
-		colors.append(color)
-	draw_polygon(polygon, colors)
+	draw_polygon(polygon, [color])
