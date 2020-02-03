93b5539a15cb4b322b9c321c98949f54a7a80dc9
Francois Belair <razoric480@gmail.com>
2020-02-03 15:18:36 -0500

    Make pirate return to spawn point after max range

--- project/src/Enemies/Pirate/PirateShip.gd
+++ project/src/Enemies/Pirate/PirateShip.gd
@@ -9,10 +9,14 @@ export var angular_acceleration_max := 15
 export var angular_drag_factor := 0.1
 export var distance_from_player_min := 200.0
 export var distance_from_obstacles_min := 200.0
+export var aggro_radius := 300.0
+export var distance_from_spawn_max := 600.0
 
 var _acceleration := GSTTargetAcceleration.new()
 var _velocity := Vector2.ZERO
 var _angular_velocity := 0.0
+var _arrive_home_blend: GSTBlend
+var _pursue_face_blend : GSTBlend
 
 onready var agent := GSTSteeringAgent.new()
 onready var player_agent: GSTSteeringAgent = get_tree().get_nodes_in_group("Player")[0].agent
@@ -27,6 +31,7 @@ onready var world_proximity := GSTRadiusProximity.new(
 		[],
 		distance_from_obstacles_min
 )
+onready var spawn_location := GSTAgentLocation.new()
 
 
 func _ready() -> void:
@@ -36,6 +41,8 @@ func _ready() -> void:
 	agent.angular_speed_max = angular_speed_max
 	agent.bounding_radius = MathUtils.get_triangle_circumcircle_radius($CollisionShape.polygon)
 	_update_agent()
+	spawn_location.position.x = global_position.x
+	spawn_location.position.y = global_position.y
 	
 	var pursue := GSTPursue.new(agent, player_agent)
 	
@@ -43,19 +50,33 @@ func _ready() -> void:
 	face.alignment_tolerance = deg2rad(5)
 	face.deceleration_radius = deg2rad(45)
 	
-	var pursue_face_blend := GSTBlend.new(agent)
-	pursue_face_blend.add(pursue, 1)
-	pursue_face_blend.add(face, 1)
+	_pursue_face_blend = GSTBlend.new(agent)
+	_pursue_face_blend.add(pursue, 1)
+	_pursue_face_blend.add(face, 1)
+	_pursue_face_blend.is_enabled = false
 	
 	var separation := GSTSeparation.new(agent, player_proximity)
 	separation.decay_coefficient = pow(player_proximity.radius, 2)/0.15
 	
-	pursue_face_blend.add(separation, 2)
+	_pursue_face_blend.add(separation, 2)
 	
 	var avoid := GSTAvoidCollisions.new(agent, world_proximity)
+
+	var arrive := GSTArrive.new(agent, spawn_location)
+	arrive.arrival_tolerance = 200
+	arrive.deceleration_radius = 300
+	var look := GSTLookWhereYouGo.new(agent)
+	look.alignment_tolerance = deg2rad(5)
+	look.deceleration_radius = deg2rad(45)
+
+	_arrive_home_blend = GSTBlend.new(agent)
+	_arrive_home_blend.add(arrive, 1)
+	_arrive_home_blend.add(look, 1)
+	_arrive_home_blend.is_enabled = false
 	
 	priority.add(avoid)
-	priority.add(pursue_face_blend)
+	priority.add(_arrive_home_blend)
+	priority.add(_pursue_face_blend)
 	
 	var world_objects := get_tree().get_nodes_in_group("World_Objects")
 	for wo in world_objects:
@@ -66,7 +87,18 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	_update_agent()
-	
+
+	var distance_from_spawn := agent.position.distance_to(spawn_location.position)
+
+	if distance_from_spawn > distance_from_spawn_max:
+		_arrive_home_blend.is_enabled = true
+		_pursue_face_blend.is_enabled = false
+	else:
+		var distance_from_player := agent.position.distance_to(player_agent.position)
+		if distance_from_player < aggro_radius:
+			_pursue_face_blend.is_enabled = true
+			_arrive_home_blend.is_enabled = false
+
 	priority.calculate_steering(_acceleration)
 	
 	_velocity = (
