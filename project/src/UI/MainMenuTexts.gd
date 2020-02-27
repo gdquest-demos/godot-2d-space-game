7d554ec357cf1f2ec0194cb6bf51cb89a8ea4afe
Nathan Lovato <nathan@gdquest.com>
2020-02-27 11:59:24 -0600

    Refactor the MainMenu

--- project/src/UI/MainMenuTexts.gd
+++ project/src/UI/MainMenuTexts.gd
@@ -1,40 +1,27 @@
-extends CenterContainer
+extends Control
 
 const EXPOSE_DURATION := 2.0
 const FADE_DURATION := 0.5
 
+onready var container := $HBoxContainer/CenterContainer
 onready var tween := $Tween
-onready var keyboard := $Keyboard
-onready var xbox := $Xbox
-onready var playstation := $Playstation
-onready var nintendo := $Nintendo
+onready var keyboard := container.get_node("AnyKey")
+onready var xbox := container.get_node("XboxA")
+onready var playstation := container.get_node("PlaystationX")
+onready var nintendo := container.get_node("NintendoB")
 
-onready var texts := [keyboard, xbox, playstation, nintendo]
+onready var elements := [keyboard, xbox, playstation, nintendo]
 
 
 func _ready() -> void:
 	var total_time := 0.0
-	for text in texts:
-		total_time += _fade_in(text, total_time)
-		total_time += _fade_out(text, total_time)
+	for element in elements:
+		total_time += _fade_in(element, total_time)
+		total_time += _fade_out(element, total_time)
 	tween.start()
 
 
-func _fade_out(target: RichTextLabel, total_time: float) -> float:
-	tween.interpolate_property(
-		target,
-		"modulate",
-		Color.white,
-		Color.transparent,
-		FADE_DURATION,
-		Tween.TRANS_LINEAR,
-		Tween.EASE_OUT,
-		total_time + EXPOSE_DURATION
-	)
-	return FADE_DURATION + EXPOSE_DURATION
-
-
-func _fade_in(target: RichTextLabel, total_time: float) -> float:
+func _fade_in(target: Control, total_time: float) -> float:
 	tween.interpolate_property(
 		target,
 		"modulate",
@@ -46,3 +33,17 @@ func _fade_in(target: RichTextLabel, total_time: float) -> float:
 		total_time
 	)
 	return FADE_DURATION
+
+
+func _fade_out(target: Control, total_time: float) -> float:
+	tween.interpolate_property(
+		target,
+		"modulate",
+		Color.white,
+		Color.transparent,
+		FADE_DURATION,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT,
+		total_time + EXPOSE_DURATION
+	)
+	return FADE_DURATION + EXPOSE_DURATION
