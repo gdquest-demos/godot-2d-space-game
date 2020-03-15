1e922a15786b2f289570ce156f3b77f24ab64777
Nathan Lovato <nathan@gdquest.com>
2020-03-14 18:54:02 -0600

    Simplify the MapSprite class

--- project/src/UI/Minimap/MapSprite.gd
+++ project/src/UI/Minimap/MapSprite.gd
@@ -1,18 +1,16 @@
 # Holds a sprite representation of itself and moves in the 
 # minimap world using a remote transform that ties it to the original.
-extends Node2D
+extends Sprite
 
 var acting_remote_transform: RemoteTransform2D
-
-onready var sprite: Sprite = $Sprite
 
 
 func setup(remote_transform: RemoteTransform2D, icon: MapIcon) -> void:
 	acting_remote_transform = remote_transform
 	remote_transform.remote_path = get_path()
-	sprite.texture = icon.texture
-	sprite.modulate = icon.color
-	sprite.scale = Vector2(icon.scale, icon.scale)
+	texture = icon.texture
+	modulate = icon.color
+	scale = Vector2(icon.scale, icon.scale)
 
 
 func clear() -> void:
