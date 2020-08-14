8c941274dd0af269ead9d35aed017ac6ba7f5e3c
Nathan Lovato <nathan@gdquest.com>
2020-08-14 08:54:07 -0600

    Add PlayerShip as a node in the Game scene
    Allows us to edit player_stats.tres in the inspector

--- project/src/World/Spawners/StationSpawner.gd
+++ project/src/World/Spawners/StationSpawner.gd
@@ -4,21 +4,20 @@ class_name StationSpawner
 extends Node2D
 
 export var Station: PackedScene
-export var Player: PackedScene
 export var radius_player_near_station := 300.0
+
+onready var player_ship: PlayerShip = $PlayerShip
 
 
 func spawn_station(rng: RandomNumberGenerator) -> void:
 	var station := Station.instance()
-	var player := Player.instance()
-	add_child(player)
 	add_child(station)
-	player.global_position = (
+	player_ship.global_position = (
 		station.global_position
 		+ (Vector2.UP.rotated(rng.randf_range(0, PI * 2)) * radius_player_near_station)
 	)
-	Events.emit_signal("station_spawned", station, player)
+	Events.emit_signal("station_spawned", station, player_ship)
 
 
 func get_player() -> PlayerShip:
-	return get_node("PlayerShip") as PlayerShip
+	return player_ship
