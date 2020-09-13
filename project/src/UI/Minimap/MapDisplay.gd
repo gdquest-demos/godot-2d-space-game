9afc322cad4ddb6e77e3ad2805f90ba3213035df
Nathan Lovato <nathan@gdquest.com>
2020-09-13 09:32:04 -0600

    Add sounds when zooming in and out on the minimap

--- project/src/UI/Minimap/MapDisplay.gd
+++ project/src/UI/Minimap/MapDisplay.gd
@@ -2,14 +2,24 @@
 # disappearing
 extends TextureRect
 
-onready var _anim_player := $AnimationPlayer
+const AUDIO_STREAMS := {
+	appear = preload("UI_Minimap_ZoomIn.wav"),
+	disappear = preload("UI_Minimap_ZoomOut.wav"),
+}
+
+onready var _anim_player: AnimationPlayer = $AnimationPlayer
+onready var _audio_player: AudioStreamPlayer = $AudioStreamPlayer
 
 
 func toggle() -> void:
 	if visible:
 		_anim_player.play("disappear")
+		_audio_player.stream = AUDIO_STREAMS.appear
+		_audio_player.play()
 	else:
 		_anim_player.play("appear")
+		_audio_player.stream = AUDIO_STREAMS.disappear
+		_audio_player.play()
 	Events.emit_signal("map_toggled", visible, _anim_player.current_animation_length)
 
 
