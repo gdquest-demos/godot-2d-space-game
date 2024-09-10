d634706029f26cc8c391096b3bfc00318c2047ad
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2024-09-10 16:28:47 +0200

    docking point supdate & refactor TweenAura

--- project/World/DockingPoint/Station.gd
+++ project/World/DockingPoint/Station.gd
@@ -3,10 +3,13 @@
 class_name Station
 extends DockingPoint
 
-export var upgrade_iron_amount := 99.0
+@export var upgrade_iron_amount := 99.0
 
-var accumulated_iron := 0.0 setget _set_accumulated_iron
+var accumulated_iron := 0.0: set = _set_accumulated_iron
 
+
+func _ready() -> void:
+	super()
 
 func _set_accumulated_iron(value: float) -> void:
 	accumulated_iron = value
--- project/World/DockingPoint/TweenAura.gd
+++ project/World/DockingPoint/TweenAura.gd
@@ -1,34 +1,7 @@
-# Animates a circular area scaling up and down. See the `DockingPoint` and its `DockingAura` for an
-# example.
+# Holds state for tweening Auras, see `DockingPoint` and its `DockingAura`.
 class_name TweenAura
-extends Tween
 
-export var scale_hidden := Vector2.ZERO
-export var scale_final := Vector2(1, 1)
-export var duration_appear := 1.0
-export var duration_disappear := 0.5
-
-
-func make_appear(aura: Sprite) -> void:
-	if is_active():
-		return
-	if aura.visible:
-		return
-	interpolate_property(
-		aura, "scale", aura.scale, scale_final, duration_appear, Tween.TRANS_ELASTIC, Tween.EASE_OUT
-	)
-	aura.visible = true
-	start()
-
-
-func make_disappear(aura: Sprite) -> void:
-	if is_active():
-		return
-	if not aura.visible:
-		return
-	interpolate_property(
-		aura, "scale", aura.scale, scale_hidden, duration_disappear, Tween.TRANS_BACK, Tween.EASE_IN
-	)
-	start()
-	yield(self, "tween_completed")
-	aura.visible = false
+@export var scale_hidden := Vector2.ZERO
+@export var scale_final := Vector2(1, 1)
+@export var duration_appear := 1.0
+@export var duration_disappear := 0.5
